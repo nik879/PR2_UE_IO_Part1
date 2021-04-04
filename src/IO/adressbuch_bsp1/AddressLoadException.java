84de66f5d8e0270482d1e50dc46a9f7a97c0c84c
DESKTOP-FISKTM0\nikik <niki.kachelmaier@gmail.com>
2021-04-04 13:32:28 +0200

    Revert "Beispiel Adressbuch fertig"
    This reverts commit 215715c1

--- src/IO/adressbuch_bsp1/AddressLoadException.java
+++ src/IO/adressbuch_bsp1/AddressLoadException.java
@@ -1,22 +1,2 @@
-package IO.adressbuch_bsp1;
-
-public class AddressLoadException extends Exception {
-    public AddressLoadException() {
-    }
-
-    public AddressLoadException(String message) {
-        super(message);
-    }
-
-    public AddressLoadException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public AddressLoadException(Throwable cause) {
-        super(cause);
-    }
-
-    public AddressLoadException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
+package IO.adressbuch_bsp1;public class AddressLoadException {
 }
